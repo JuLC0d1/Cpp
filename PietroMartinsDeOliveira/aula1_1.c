19c714e3d79c39cf7c250b2926625e89683c07ee
Juliana <jd391829@gmail.com>
2023-09-02 13:04:29 -0300

    02/09/23
    inicio de curso de C

---
+++ PietroMartinsDeOliveira/aula1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> // o system pause vem daqui, (no c++ n precisa desta biblioteca mas enfim)
+
+int main() {
+    printf("Hello, World!\n");
+
+    system("pause");
+}
+
+/*
+O código 0 de 'return 0' é tipo um 'OK' q o computador/windows da, para dizer q o código n deu erro algum, então rodou 100%. No caso, se ao invez de 0 aparecer outro número, quer dizer que está com algum problema. 
+
+se der erro no código, primeira coisa: veja se esta faltando algum ponto e virgula
+*/
---
+++ PietroMartinsDeOliveira/aula1_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv[]) {
+
+    printf("Mensagem 1.\n");
+    printf("Mensagem 2.\n");
+    printf("Mensagem 3.\n");
+    printf("Mensagem 4.\n");
+    printf("Mensagem 5.\n");
+    printf("Mensagem 6.\n");
+    printf("Mensagem 7.\n");
+    printf("Mensagem 8.\n");
+    printf("Mensagem 9.\n");
+    printf("Mensagem 10.\n");
+
+
+    return 0;
+    system("pause");
+}
